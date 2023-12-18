cff1681e72e2fceaf45f7d4397a3f200ed284860
DIEG0MARIN <diegof.maring@konradlorenz.edu.co>
2023-12-17 19:29:49 -0500

    se realizan ajustes de la interfaz de prestamo

--- djangoCRUD/models.py
+++ djangoCRUD/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
 class Prestamos(models.Model):
-    id = models.AutoField(primary_key=True)
-    nombres = models.CharField(max_length = 30)
-    perfil = models.CharField(max_length = 30)
-    telefono = models.CharField(max_length = 20)
-    correo = models.CharField(max_length = 50)
-    facultad = models.CharField(max_length = 40)
-    numeroPortatil = models.CharField(max_length = 10)
-    fecha = models.CharField(max_length = 40)
-    observaciones = models.CharField(max_length = 100)
+    id = models.IntegerField(primary_key=True)
+    nombres = models.CharField(max_length=30)
+    perfil = models.CharField(max_length=30)
+    telefono = models.IntegerField()  # Cambiado de CharField a IntegerField
+    correo = models.CharField(max_length=50)
+    facultad = models.CharField(max_length=40)
+    numeroPortatil = models.IntegerField()
+    fecha = models.IntegerField()
+    observaciones = models.CharField(max_length=100)
+
+    def __str__(self): 
+        text = "{0} {1}"
+        return text.format(self.id ,self.nombres)
